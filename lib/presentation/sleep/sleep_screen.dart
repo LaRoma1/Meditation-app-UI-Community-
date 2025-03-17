e4bf20c5096c21a3191d9b757276a40db491c721
romaricte <tenrom142@gmail.com>
2025-03-17 22:52:56 +0100

    Ajout de la navigation par onglets dans l'écran d'accueil et mise à jour des écrans de méditation, de sommeil et de profil pour utiliser des widgets Stateless.
    - Implémentation d'une barre de navigation inférieure avec des éléments interactifs
    - Ajout de la couleur primaire dans AppColors
    - Révision des écrans de méditation, de sommeil et de profil avec un design de base
    - Mise à jour des routes pour inclure les nouveaux écrans

--- lib/presentation/sleep/sleep_screen.dart
+++ lib/presentation/sleep/sleep_screen.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:meditation_app_ui_community/common/widgets/custom_text.dart';
+import 'package:meditation_app_ui_community/core/utils/colors.dart';
 
-class SleepScreen extends StatefulWidget {
+class SleepScreen extends StatelessWidget {
   const SleepScreen({super.key});
 
   @override
-  State<SleepScreen> createState() => _SleepScreenState();
-}
-
-class _SleepScreenState extends State<SleepScreen> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomText(
+                text: 'Sommeil',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              SizedBox(height: 20),
+              // Ajoutez ici le contenu de votre écran de sommeil
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
