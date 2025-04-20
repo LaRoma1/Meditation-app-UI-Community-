d5b1d73a2e69476e788f7dc390d74bfc30031edc
romaricte <tenrom142@gmail.com>
2025-04-20 14:36:39 +0100

    Refonte de l'écran de sommeil : transformation de StatelessWidget en StatefulWidget, ajout de filtres horizontaux pour la sélection, mise à jour du texte et amélioration de l'interface utilisateur avec un nouveau design et des éléments interactifs.

--- lib/presentation/sleep/sleep_screen.dart
+++ lib/presentation/sleep/sleep_screen.dart
@@ -3,55 +3,200 @@ import 'package:meditation_app_ui_community/common/widgets/custom_text.dart';
 import 'package:meditation_app_ui_community/core/constants/image_constants.dart';
 import 'package:meditation_app_ui_community/core/utils/colors.dart';
 
-class SleepScreen extends StatelessWidget {
+class SleepScreen extends StatefulWidget {
   const SleepScreen({super.key});
 
   @override
+  State<SleepScreen> createState() => _SleepScreenState();
+}
+
+class _SleepScreenState extends State<SleepScreen> {
+  int _selectedIndex = 0;
+
+  final List<Map<String, dynamic>> _filterItems = [
+    {
+      'icon': Icons.add,
+      'label': 'All',
+      'color': AppColors.primaryColor,
+      'isSelected': true,
+    },
+    {
+      'icon': Icons.favorite_outline,
+      'label': 'My',
+      'color': Colors.grey,
+      'isSelected': false,
+    },
+    {
+      'icon': Icons.sentiment_dissatisfied_outlined,
+      'label': 'Anxious',
+      'color': Colors.grey,
+      'isSelected': false,
+    },
+    {
+      'icon': Icons.nightlight_round,
+      'label': 'Sleep',
+      'color': Colors.grey,
+      'isSelected': false,
+    },
+    {
+      'icon': Icons.child_care_outlined,
+      'label': 'Kids',
+      'color': Colors.grey,
+      'isSelected': false,
+    },
+  ];
+
+  @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            
             image: AssetImage(ImageConstants.welcomesleep),
             fit: BoxFit.cover,
           ),
         ),
-         child: SafeArea(
+        child: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: 20),
                 CustomText(
-                  text: 'Welcome to Sleep',
-                  fontSize: 24,
+                  text: 'Sleep Stories',
                   color: Colors.white,
+                  fontSize: 24,
                   fontWeight: FontWeight.bold,
                 ),
-                SizedBox(height: 14),
+                SizedBox(height: 20),
                 CustomText(
                   textAlign: TextAlign.center,
-                  text: 'Explore the new king of sleep. It uses sound and vesualization to create perfect conditions for refreshing sleep.',
+                  text:
+                      'Soothing bedtime stories to help you fall into a deep and natural sleep',
                   fontSize: 14,
-                  color: Colors.white,
+                  color: AppColors.grey,
                 ),
-                
                 SizedBox(height: 20),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Image.asset(
-                    ImageConstants.birds,
-                    fit: BoxFit.contain,
+
+                // Filtres horizontaux
+                SizedBox(
+                  height: 90,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: _filterItems.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selectedIndex = index;
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(right: 15),
+                          width: 65,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 60,
+                                width: 60,
+                                decoration: BoxDecoration(
+                                  color:
+                                      index == _selectedIndex
+                                          ? _filterItems[index]['color']
+                                          : Colors.grey.shade400,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Icon(
+                                  _filterItems[index]['icon'],
+                                  color: Colors.white,
+                                  size: 24,
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                              CustomText(
+                                text: _filterItems[index]['label'],
+                                fontSize: 12,
+                                color:
+                                    index == _selectedIndex
+                                        ? Colors.black
+                                        : Colors.grey,
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
+
                 SizedBox(height: 20),
-                CustomText(
-                  text: 'Get started',
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal,
+
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          ImageConstants.bannersleep,
+                          fit: BoxFit.cover,
+                          width: MediaQuery.of(context).size.width,
+                          height: MediaQuery.of(context).size.height * 0.25,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                CustomText(
+                                  text: 'The ocean moon',
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                SizedBox(height: 8),
+                                CustomText(
+                                  text:
+                                      'Non-stop 8-hour mixes of our\nmost popular sleep audio',
+                                  color: Colors.white70,
+                                  fontSize: 14,
+                                ),
+                              ],
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: AppColors.primaryColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 20,
+                                  vertical: 12,
+                                ),
+                              ),
+                              child: Text(
+                                'START',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
